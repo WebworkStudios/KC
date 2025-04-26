f23b53b3f0e122e660693c614eac01a7c0856a2a
Jan Steiger <j.steiger@gmx.de>
2025-04-26 19:58:08 +0200

    New

--- app/Actions/HelloWorldAction.php
+++ app/Actions/HelloWorldAction.php
@@ -3,9 +3,11 @@ namespace App\Actions;
 
 use Src\Http\Request;
 use Src\Http\Response;
+use Src\Http\Route;
 
 class HelloWorldAction
 {
+    #[Route(path: '/', name: 'home')]
     public function __invoke(Request $request): Response
     {
         return new Response(view('hello_world', [
